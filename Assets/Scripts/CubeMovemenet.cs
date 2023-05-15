35c423bace0048b49269a41f7374e04d6fbfcdd0
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2023-05-15 18:46:41 +0600

    New_Script_Cube

---
+++ Assets/Scripts/CubeMovemenet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ongar
+{
+    public class CubeMovemenet : MonoBehaviour
+    {
+        public float speed = 2f;
+        public float moveDistance = 2f;
+
+        private bool isMovingLeft = true;
+
+        private void Update()
+        {
+            float moveAmount = isMovingLeft ? -moveDistance : moveDistance;
+            Vector3 movement = new Vector3(moveAmount, 0f, 0f) * speed * Time.deltaTime;
+            transform.Translate(movement);
+
+            if (Mathf.Abs(transform.position.x) >= moveDistance)
+            {
+                isMovingLeft = !isMovingLeft;
+            }
+        }
+    }
+}
+
+
