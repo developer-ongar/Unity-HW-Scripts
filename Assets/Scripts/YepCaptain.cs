23547f41c024512939efe3ddb58b24e5ff1179e9
second-ongardev <120540001+second-ongardev@users.noreply.github.com>
2023-05-15 18:03:52 +0600

    First Script

---
+++ Assets/Scripts/YepCaptain.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ongar
+{
+    public class YepCaptain : MonoBehaviour
+    {
+        public Text captainAlex;
+
+        void Start()
+        {
+            captainAlex.text = "Я готов, капитан";
+        }
+    }
+}
+
+
+
